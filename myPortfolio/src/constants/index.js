cbe1c82739bdd612f02668b1940860223c7cbcb3
krishnashuk <shuklakrishna.kris@gmail.com>
2024-10-13 20:22:50 +0530

    Initial commit for React portfolio project

---
+++ myPortfolio/src/constants/index.js
@@ -0,0 +1,54 @@
+import project1 from "../assets/project-1.jpg";
+import project3 from "../assets/project-3.jpg";
+import project4 from "../assets/project-4.jpg";
+
+export const HERO_CONTENT = ` I'm Krishna, a passionate React developer dedicated to building dynamic, user-friendly web applications.
+With a strong foundation in JavaScript and expertise in React, I specialize in creating responsive, high-performance interfaces that provide seamless user experiences. I enjoy solving complex problems and continually learning new technologies to refine my skills. Whether it's collaborating with teams or working independently, I’m committed to writing clean, efficient code and bringing innovative ideas to life. Check out my projects below to see what I've been working on!`;
+
+export const ABOUT_TEXT = `Hi! I'm Krishna, a passionate React Frontend Engineer dedicated to creating intuitive, high-performance web applications.\n
+With a strong foundation in JavaScript and extensive experience in building responsive and dynamic user interfaces, I focus on delivering seamless experiences for users.\n
+My expertise lies in leveraging the power of React to develop scalable, maintainable, and efficient front-end systems.\n
+\n
+I enjoy turning complex problems into simple, beautiful, and functional web solutions. My toolkit includes modern technologies like React,TypeScript, and Tailwind CSS, among others.\n
+
+\n
+Constantly striving to improve my skills and stay updated with the latest technologies, I love collaborating with others and learning from every project.\n
+Whether I'm building single-page applications or contributing to open-source projects, my goal is to create clean, efficient, and user-friendly code.\n
+\n
+When I'm not coding, you can find me exploring new tools, diving into tech blogs, or working on side projects that push my creativity.\n
+I’m excited about the endless possibilities in the web development space and look forward to contributing to projects that make an impact.\n
+\n
+Let’s build something great together!`;
+
+
+
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "AKG ENGINEERING COLLEGE ,Ghaziabad ",
+  email: "Shuklakrishna.kris@gmail.com",
+};
